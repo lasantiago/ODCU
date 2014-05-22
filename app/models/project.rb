3befc49c23b24599b1c3e4bac2859a5c69d11328
Luis Santiago <luis.santiago@gmail.com>
2014-05-22 10:58:45 -0400

    initial commit on GIT

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,4 @@
 class Project < ActiveRecord::Base
-	
-	validates :name, presence: true, length: { minimun: 5 }
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
+
+  get 'terms/index'
+
+  get 'about/index'
+
+  get 'welcome/index'
+
+  resources :projects
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   get 'project/index'
   # Example of regular route:
